176331b607a596d53881f5eaa1f03a1026e0dd39
ElePhontitis <menke.daniel@live.de>
2015-12-10 13:05:38 +0100

    ""

---
+++ src/bpm/finder/WavFilePlotter.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bpm.finder;
+
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author Ele
+ */
+public class WavFilePlotter  {
+    Canvas canvas = new Canvas(300,250);
+    GraphicsContext gc = canvas.getGraphicsContext2D();
+    
+ 
+    public void plotData (Point2D point){
+        gc.setFill(Color.WHITE);
+        gc.strokeLine(40, 10, 10, 40);
+        
+    }
+
+    public Canvas getPanel(){
+        return this.canvas;
+    }
+    
+}
